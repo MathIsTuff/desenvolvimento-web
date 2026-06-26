057c9ad7f9e20642c24d3eb671d4638ca92e6ad8
MathIsTuff <mathistuff@gmail.com>
2026-06-26 15:32:48 -0300

    Guia 02 - Section Hero

--- segundobi/avaliativo/projetoWeb/src/pages/Home.tsx
+++ segundobi/avaliativo/projetoWeb/src/pages/Home.tsx
@@ -5,6 +5,9 @@ import Close from "../assets/close.svg";
 import { useState, useEffect } from "react";
 import "../styles/header.css";
 import "../styles/utility.css";
+import HeroRectangleOne from "../assets/images/rectangleOne.png";
+import HeroRectangleTwo from "../assets/images/rectangleTwo.png";
+import "../styles/hero.css";
 
 
 export default function Home() {
@@ -13,69 +16,93 @@ export default function Home() {
         <>
             <header className="container py-sm">
                 <nav className="flex items-center justify-between">
-    <img src={Logo} alt="Logo Arcitech" width={220} height={200} />
-    
-    <div className="desktop-only">
-    <ul className="flex gap-1">
-        <li>
-            <a href="#">Home</a>
-        </li>
-        <li>
-            <a href="#solution">Soluções</a>
-        </li>
-        <li>
-            <a href="#testimonials">Depoimentos</a>
-        </li>
-        <li>
-            <a href="#pricing">Preços</a>
-        </li>
-        <li>
-            <a href="#contact">Contato</a>
-        </li>
-    </ul>
-</div>
+                    <img src={Logo} alt="Logo Arcitech" width={220} height={200} />
 
-<div className="desktop-only">
-    <div className="flex items-center">
-        <a className="reverse-color ml-lg" href="">Login</a>
-        <Button text="Cadastre-se" />
-    </div>
-</div>
-<div className="mobile-menu">
-    {showMobileMenu ?
-        <div className="mobile-menu-content">
-            <div className="container flex">
-                <ul>
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#solution">Soluções</a>
-                    </li>
-                    <li>
-                        <a href="#testimonials">Depoimentos</a>
-                    </li>
-                    <li>
-                        <a href="#pricing">Preços</a>
-                    </li>
-                    <li>
-                        <a href="#contact">Contato</a>
-                    </li>
-                </ul>
-                <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper">
-                    <img src={Close} alt="ícone fechar menu" width={24} height={24} />
-                </span>
-            </div>
-        </div>
-        :
-        <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper" >
-            <img src={Menu} alt="ícone menu" width={24} height={24} />
-        </span>
-    }
-</div>
+                    <div className="desktop-only">
+                        <ul className="flex gap-1">
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#solution">Soluções</a>
+                            </li>
+                            <li>
+                                <a href="#testimonials">Depoimentos</a>
+                            </li>
+                            <li>
+                                <a href="#pricing">Preços</a>
+                            </li>
+                            <li>
+                                <a href="#contact">Contato</a>
+                            </li>
+                        </ul>
+                    </div>
 
-</nav>//Essa tag nav fecha lá em baixo com o header
+                    <div className="desktop-only">
+                        <div className="flex items-center">
+                            <a className="reverse-color ml-lg" href="">Login</a>
+                            <Button text="Cadastre-se" />
+                        </div>
+                    </div>
+                    <div className="mobile-menu">
+                        {showMobileMenu ?
+                            <div className="mobile-menu-content">
+                                <div className="container flex">
+                                    <ul>
+                                        <li>
+                                            <a href="#">Home</a>
+                                        </li>
+                                        <li>
+                                            <a href="#solution">Soluções</a>
+                                        </li>
+                                        <li>
+                                            <a href="#testimonials">Depoimentos</a>
+                                        </li>
+                                        <li>
+                                            <a href="#pricing">Preços</a>
+                                        </li>
+                                        <li>
+                                            <a href="#contact">Contato</a>
+                                        </li>
+                                        <li>
+                                            <a className="reverse-color" href="#">Login</a>
+                                        </li>
+                                    </ul>
+                                    <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper">
+                                        <img src={Close} alt="ícone fechar menu" width={24} height={24} />
+                                    </span>
+                                </div>
+                            </div>
+                            :
+                            <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper" >
+                                <img src={Menu} alt="ícone menu" width={24} height={24} />
+                            </span>
+                        }
+                    </div>
+
+                </nav>
             </header>
+
+            <section id="hero">
+                <span className="desktop-only">
+                    <img src={HeroRectangleTwo} alt="Retangulo um tela inicial" />
+                </span>
+                <img src={HeroRectangleOne} alt="Retangulo dois tela inicial" />
+                <div className="container content">
+                    <p className="desktop-only">
+                        Olá
+                    </p>
+                    <h1>Comida de mãe direto no seu apê, é só pedir que entregamos para você!</h1>
+                    <p>Já pensou em matar a saudade daquela comida caseira? O melhor de tudo, nossas receitas são 100% saudáveis, bora entrar no shape.
+                    </p>
+                    <div className="flex gap-1">
+                        <span><Button text="Cadastre-se" /></span>
+                        <span className="desktop-only">
+                            <Button text="Veja mais" secondary />
+                        </span>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
